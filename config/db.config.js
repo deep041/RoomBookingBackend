1149f46fcd2f0627aba68f4f454d4a45f3d39415
Deep Patel <patel.deep041@gmail.com>
2024-04-28 15:07:34 +0530

    Error handle added

--- config/db.config.js
+++ config/db.config.js
@@ -4,5 +4,9 @@ const { MONGO_URL } = process.env;
 exports.connect = () => {
     mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log('Database connection failed!', err));    
+    .catch(err => {
+        console.log('Database connection failed!', err);
+        console.error(error);
+        process.exit(1);
+    });    
 };
