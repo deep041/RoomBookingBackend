23a8cac428065b1d263b8cef359c06e8228f3bb6
Deep Patel <patel.deep041@gmail.com>
2024-04-28 14:41:14 +0530

    PORT Changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const routes = require('./routes');
-const PORT = 3000;
+const PORT = process.env.API_PORT;
 
 app.get('/', (req, res) => {
     res.send('Project Started');
